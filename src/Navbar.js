dc1fb07c653767e4f149496fc2c85084d53e5e99
Will <widiep@gmail.com>
2020-02-04 10:19:27 -0800

    Add navbar. Need to fix right alignment with CTA container

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
+import './Navbar.scss'
 
 class Navbar extends Component {
   constructor() {
     super()
-
   }
 
   render() {
     return (
       <div>
-        FROM NAVBAR
-
+        <header>
+          <nav className='nav'>
+            <a className='nav__item nav__logo' href='#'>ParkingRabbit</a>
+            <a className='nav__item nav__item--right nav__login ' href='#'>Login</a>
+            <a className='nav__item nav__item--right nav__signup' href='#'>Sign Up</a>
+          </nav>
+        </header>
       </div>
     )
   }
