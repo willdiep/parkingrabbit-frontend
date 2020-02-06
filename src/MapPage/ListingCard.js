e29c8ff3d569cbf68c009450034082b234f7f543
Will <widiep@gmail.com>
2020-02-05 16:42:26 -0800

    Add stores.json, ListingCard, and MapPage

---
+++ src/MapPage/ListingCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import './MapPage.scss'
+
+const ListingCard = props => {
+  return (
+    <div id={`listing-${props.id}`} className="item">
+      {/* {console.log(props.store)} */}
+      <a href="#" id={`link-${props.id}`} className="title" onClick={() => {props.handleClick(props.store, props.id)}}>
+        {props.store.properties.address}
+      </a>
+      <div>
+        {props.store.properties.city}
+        {props.store.properties.phone ? ' · ' + props.store.properties.phoneFormatted: null}
+        <br></br>
+        {`$${props.store.properties.price}`}  
+      </div>
+    </div>
+  )
+}
+
+export default ListingCard
