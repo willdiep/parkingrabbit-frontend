a62ad575fc5b41e47db0b81bb102b13fda3bc4e7
Will <widiep@gmail.com>
2020-02-06 02:06:32 -0800

    Create basic style Login component. Will need to add submit button

--- src/LoginSignUp/Login.js
+++ src/LoginSignUp/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import './Login.scss'
+import googleSigninBtn from '../images/sign-in-google.png'
+import facebookContinueBtn from '../images/continue-with-facebook-btn.png'
 
 class Login extends Component {
   constructor() {
@@ -8,8 +10,55 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
-          FROM LOGIN
+      <div className='login'>
+        <main className='login__modal'>
+          <article className='login__container'>
+            <section className='login__header'>
+              <p>Login</p>
+            </section>
+
+            <br></br>
+            <br></br>
+
+            <section className='login__socialmedia'>
+              <img className='login__google' src={googleSigninBtn} />
+              <br></br>
+              <br></br>
+              <img className='login__google' src={facebookContinueBtn} />
+            </section>
+
+            <br></br>
+            <br></br>
+
+            <section className='login__horizontalruler'>
+              <div className='login__line'></div>
+              <div className='login__line-text'>OR</div>
+              <div className='login__line'></div>
+            </section>
+
+            <br></br>
+            <br></br>
+
+            <section className='login__form'>
+              <form>
+                <label>Email</label>
+                <br></br>
+                <input type='text'></input>
+                
+                <br></br>
+                <br></br>
+
+                <label>Password</label>
+                <br></br>
+                <input type='text'></input>
+              </form>
+            </section>
+
+            <section className='login__disclosure'></section>
+
+            <section className='login__register'></section>
+          </article>
+        </main>
       </div>
     )
   }
