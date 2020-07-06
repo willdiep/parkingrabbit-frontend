1b0b5b051c856bfd03767e4e8dbe4c923af28fcf
Will <widiep@gmail.com>
2020-07-06 01:12:05 -0700

    TEST

--- src/LoginSignUp/LoginSignup.js
+++ src/LoginSignUp/LoginSignup.js
@@ -8,7 +8,15 @@ class LoginSignup extends Component {
   render() {
     return (
       <>
-
+        <Navbar {...this.props} className='homepage__navbar' homePage={true} />
+        <article className='LoginSignup'>
+          <section className='LoginSignup__Banner'>
+            <figure>
+              <img className='homepage__carrotcar' src={heroimage} alt='hero' />
+            </figure>
+          </section>
+          {/* <section className='LoginSignup__Panel'>{this.props.children}</section> */}
+        </article>
       </>
     )
   }
