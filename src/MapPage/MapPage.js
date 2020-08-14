406d944a98ad04c831074041aadec5d0b621823d
willdiep <widiep@gmail.com>
2020-08-13 19:57:52 -0700

    Set listingsUrl variable to heroku

--- src/MapPage/MapPage.js
+++ src/MapPage/MapPage.js
@@ -86,8 +86,8 @@ class MapPage extends Component {
 
   fetchListings = () => {
     this.setState({ isFetching: true })
-    // const listingsUrl = 'https://parkingrabbit-backend.herokuapp.com/listings'
-    const listingsUrl = 'http://localhost:3000/listings'
+    const listingsUrl = 'https://parkingrabbit-backend.herokuapp.com/listings'
+    // const listingsUrl = 'http://localhost:3000/listings'
     fetch(listingsUrl)
       .then((response) => response.json())
       .then((result) => {
